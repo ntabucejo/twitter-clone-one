7bfa616a84c5e1aa7f05fb2076beac51b60dcea4
ntabucejo <ntabucejo@gmail.com>
2022-05-30 16:06:32 +0800

    Add trends data

---
+++ src/library/data/trends.ts
@@ -0,0 +1,66 @@
+import Trend from '../types/trend'
+
+const trends: Trend[] = [
+  {
+    id: '1',
+    title: 'CAT 7',
+    category: 'Trending in Philippines',
+    tweetCount: '2,236k',
+  },
+  {
+    id: '2',
+    title: 'CAT 2',
+    category: 'Trending in Philippines',
+    tweetCount: '10.9K',
+  },
+  {
+    id: '3',
+    title: 'Bill Gates',
+    category: 'Business & finance · Trending',
+    tweetCount: '34.9K Tweets',
+  },
+  {
+    id: '4',
+    title: 'Hotdog',
+    category: 'Trending in Philippines',
+    tweetCount: '10.7K',
+  },
+  {
+    id: '5',
+    title: 'Extraordinary You',
+    category: 'Trending in Philippines',
+    tweetCount: '5,377k',
+  },
+  {
+    id: '6',
+    title: 'President-elect',
+    category: 'Trending in Philippines',
+    tweetCount: '167K',
+  },
+  {
+    id: '7',
+    title: 'Mandaluyong City',
+    category: 'rending in Philippines',
+    tweetCount: '581K',
+  },
+  {
+    id: '8',
+    title: 'Japan',
+    category: 'Music · Trending',
+    tweetCount: '34K',
+  },
+  {
+    id: '9',
+    title: 'raine',
+    category: 'Trending in Philippines',
+    tweetCount: '6,197k',
+  },
+  {
+    id: '10',
+    title: '#SB19',
+    category: 'Music · Trending',
+    tweetCount: '40.9K',
+  },
+]
+
+export default trends
---
+++ src/library/types/trend.ts
@@ -0,0 +1,8 @@
+type Trend = {
+  id: string
+  title: string
+  category: string
+  tweetCount: string
+}
+
+export default Trend
