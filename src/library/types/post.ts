b4dd10dceb2504997da0127cf942113755a8a1c2
ntabucejo <ntabucejo@gmail.com>
2022-05-30 15:59:42 +0800

    Change directory

---
+++ src/library/types/post.ts
@@ -0,0 +1,13 @@
+type Post = {
+  id: string
+  caption: string
+  name: string
+  username: string
+  image: string
+  time: string
+  replies: number
+  retweets: number
+  likes: number
+}
+
+export default Post
