a5f047c2d5b38ba45b2b8b8042cb24d6cfb3f9a1
ntabucejo <ntabucejo@gmail.com>
2022-05-30 13:39:14 +0800

    Update layout

--- src/components/publisher/index.tsx
+++ src/components/publisher/index.tsx
@@ -12,7 +12,7 @@ import Textbox from './textbox'
 
 const Publisher = () => {
   return (
-    <section className="px-4 py-4 flex gap-4 ">
+    <section className="px-4 py-4 grid grid-cols-[auto,1fr] gap-4 ">
       <Avatar
         src="https://images.unsplash.com/photo-1614639437280-558b05b13939?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDM0fHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
         alt="Profile"
