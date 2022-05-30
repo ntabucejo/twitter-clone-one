0f7e9a204ce79cbfff1de3065461748ee9c3ebae
ntabucejo <ntabucejo@gmail.com>
2022-05-30 14:49:12 +0800

    Add posts data

---
+++ src/components/library/data/posts.ts
@@ -0,0 +1,132 @@
+const posts = [
+  {
+    id: '1',
+    caption:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    name: 'Philip Myrtorp',
+    username: 'philipmyrtorp',
+    image:
+      'https://images.unsplash.com/photo-1602489053809-4d912f6c8b4d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE3fHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    time: '2h',
+    replies: 32,
+    retweets: 12,
+    likes: 321,
+  },
+  {
+    id: '2',
+    caption:
+      'fringilla urna porttitor rhoncus. Cras adipiscing enim eu turpis egestas. Fringilla est ullamcorper eget nulla facilisi etiam dignissim. Turpis tincidunt id aliquet risus feugiat in. Ullamcorper morbi tincidunt ornare massa eget egestas purus viverra. Risus nec feugiat in fermentum posuere urna nec tincidunt praesent. Id faucibus nisl tincidunt eget nullam non nisi est sit. Vitae sapien pellentesque habitant morbi tristique senectus et netus',
+    name: 'Jasmin Chew',
+    username: 'jnchew',
+    image:
+      'https://images.unsplash.com/photo-1639337916963-e16623394e63?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDM2fHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    time: '13h',
+    replies: 2,
+    retweets: 13,
+    likes: 76,
+  },
+  {
+    id: '3',
+    caption:
+      'Faucibus interdum posuere lorem ipsum. Donec pretium vulputate sapien nec. Leo vel orci porta non pulvinar neque. Mattis vulputate enim nulla aliquet.',
+    name: 'Andre Hunter',
+    username: 'andrehunter',
+    image:
+      'https://images.unsplash.com/photo-1528980917907-8df7f48f6f2a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDU1fHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    time: '5h',
+    replies: 32,
+    retweets: 62,
+    likes: 85,
+  },
+  {
+    id: '4',
+    caption:
+      'Nullam ac tortor vitae purus. Elit ullamcorper dignissim cras tincidunt. Consequat ac felis donec et odio pellentesque diam volutpat commodo.',
+    name: 'Zane Bolen',
+    username: 'zanebolen',
+    image:
+      'https://images.unsplash.com/photo-1616085258995-85bd229048d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDcxfHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    time: '4h',
+    replies: 3,
+    retweets: 21,
+    likes: 87,
+  },
+  {
+    id: '5',
+    caption: '',
+    name: 'Benjamin Hersh',
+    username: 'benhersh',
+    image:
+      'https://images.unsplash.com/photo-1651986491787-b6a2fbbb5101?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDk0fHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    time: '8h',
+    replies: 23,
+    retweets: 86,
+    likes: 12,
+  },
+  {
+    id: '6',
+    caption:
+      'consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla posuere sollicitudin aliquam ultrices sagittis orci. Egestas egestas fringilla phasellus faucibus scelerisque eleifend donec.',
+    name: 'Freddie Addery',
+    username: 'freddieaddery',
+    image:
+      'https://images.unsplash.com/photo-1652079129755-97b606e144ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIzN3x0b3dKWkZza3BHZ3x8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+    time: '2h',
+    replies: 65,
+    retweets: 87,
+    likes: 151,
+  },
+  {
+    id: '7',
+    caption: '',
+    name: 'Emanuel Turbuc',
+    username: 'emanuelturbuc',
+    image:
+      'https://images.unsplash.com/photo-1652549752120-d9beb4c86bd4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDEwM3x0b3dKWkZza3BHZ3x8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+    time: '1hr',
+    replies: 1,
+    retweets: 67,
+    likes: 103,
+  },
+  {
+    id: '8',
+    caption:
+      'Donec adipiscing tristique risus nec feugiat in fermentum posuere urna. Eu turpis egestas pretium aenean pharetra magna ac placerat. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Lorem sed risus ultricies tristique nulla aliquet enim tortor. Vivamus at augue eget arcu dictum varius duis at.',
+    name: 'Akindele Ibukun',
+    username: 'akind_bukun',
+    image:
+      'https://images.unsplash.com/photo-1652437225670-f7969e367375?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE1NXx0b3dKWkZza3BHZ3x8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+    time: '7h',
+    replies: 51,
+    retweets: 5,
+    likes: 2,
+  },
+  {
+    id: '9',
+    caption:
+      'Semper risus in hendrerit gravida. Aliquam sem et tortor consequat id porta nibh venenatis cras',
+    name: 'Davide Aracri',
+    username: 'davidearacri',
+    image:
+      'https://images.unsplash.com/photo-1652226569981-00087ea729b2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDE2NHx0b3dKWkZza3BHZ3x8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+    time: '7h',
+    replies: 2,
+    retweets: 8,
+    likes: 65,
+  },
+  {
+    id: '10',
+    caption:
+      'Est lorem ipsum dolor sit amet consectetur adipiscing elit. Quis hendrerit dolor magna eget.',
+    name: 'Lucas Andrade',
+    username: 'lucasandrade',
+    image:
+      'https://images.unsplash.com/photo-1651850600560-64b7d701cd3d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIwN3x0b3dKWkZza3BHZ3x8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+    time: '15h',
+    replies: 862,
+    retweets: 231,
+    likes: 312,
+  },
+]
+
+export default posts
